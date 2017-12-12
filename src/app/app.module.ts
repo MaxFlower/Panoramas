94941234fd59bb77cee723d5ae3bdd1a6d1a5fd6
maxim.tsvetkov <maxtsvetapple@gmail.com>
2017-12-12 23:34:26 +0200

    Panoramas: Created structure for project. Added main header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { PanoramasModule } from './panoramas/panoramas.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { MainSidebarComponent } from './components/main-sidebar/main-sidebar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SelectorComponent } from './common/components/selector/selector.component';
+import { GridItemComponent } from './common/components/items/grid-item/grid-item.component';
+import { ListItemComponent } from './common/components/items/list-item/list-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    MainContentComponent,
+    SelectorComponent,
+    GridItemComponent,
+    ListItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PanoramasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
